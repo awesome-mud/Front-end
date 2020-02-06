300fe1dfb56ffc5ee7e88d506e5e73b9dd26494a
Ty Lippe <tylippe94@gmail.com>
2020-02-06 16:56:42 -0500

    starting to mess with music

--- thunderball/src/view/mainPage.js
+++ thunderball/src/view/mainPage.js
@@ -4,14 +4,14 @@ import Initialize from '../components/initialize'
 import Move from '../components/move'
 import Map from '../components/map'
 import Logo from '../images/logo.png'
+import Song from '../music.mp3'
 
 const MainPage = () => {
-
-
 
     return (
         <div className='mainDiv'>
             <img src={Logo} alt='logo' className='logo'/>
+            <audio controls src={Song} volume='0.01'/>
             <div className='contentDiv'>
             <div className='leftDiv'>
             <div className='init'>
